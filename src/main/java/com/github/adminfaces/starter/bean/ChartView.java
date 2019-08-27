ae89d9fe72bd8c1e0d65d959f920b26e339e98ed
EmreUlgac <ulgacemre@gmail.com>
2019-08-27 22:29:49 +0300

    v-1

---
+++ src/main/java/com/github/adminfaces/starter/bean/ChartView.java
@@ -0,0 +1,355 @@
+package com.github.adminfaces.starter.bean;
+
+import org.primefaces.event.ItemSelectEvent;
+import org.primefaces.model.chart.*;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
+
+@ManagedBean
+public class ChartView implements Serializable {
+
+    private LineChartModel lineModel1;
+    private LineChartModel lineModel2;
+    private LineChartModel zoomModel;
+    private CartesianChartModel combinedModel;
+    private CartesianChartModel fillToZero;
+    private LineChartModel areaModel;
+    private BarChartModel barModel;
+    private HorizontalBarChartModel horizontalBarModel;
+    private PieChartModel pieModel1;
+    private PieChartModel pieModel2;
+    private DonutChartModel donutModel1;
+    private DonutChartModel donutModel2;
+    private MeterGaugeChartModel meterGaugeModel1;
+    private MeterGaugeChartModel meterGaugeModel2;
+    private BubbleChartModel bubbleModel1;
+    private BubbleChartModel bubbleModel2;
+    private OhlcChartModel ohlcModel;
+    private OhlcChartModel ohlcModel2;
+    private PieChartModel livePieModel;
+    private LineChartModel animatedModel1;
+    private BarChartModel animatedModel2;
+    private LineChartModel multiAxisModel;
+    private LineChartModel dateModel;
+
+    @PostConstruct
+    public void init() {
+
+        createAreaModel();
+
+
+        createOhlcModels();
+
+        createBarModels();
+
+        createCombinedModel();
+        createMultiAxisModel();
+
+    }
+
+
+
+    public void itemSelect(ItemSelectEvent event) {
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Item selected",
+                "Item Index: " + event.getItemIndex() + ", Series Index:" + event.getSeriesIndex());
+
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public LineChartModel getLineModel1() {
+        return lineModel1;
+    }
+
+    public LineChartModel getLineModel2() {
+        return lineModel2;
+    }
+
+    public LineChartModel getZoomModel() {
+        return zoomModel;
+    }
+
+    public CartesianChartModel getCombinedModel() {
+        return combinedModel;
+    }
+
+    public CartesianChartModel getAreaModel() {
+        return areaModel;
+    }
+
+    public PieChartModel getPieModel1() {
+        return pieModel1;
+    }
+
+    public PieChartModel getPieModel2() {
+        return pieModel2;
+    }
+
+    public BarChartModel getBarModel() {
+        return barModel;
+    }
+
+    public HorizontalBarChartModel getHorizontalBarModel() {
+        return horizontalBarModel;
+    }
+
+    public LineChartModel getAnimatedModel1() {
+        return animatedModel1;
+    }
+
+    public BarChartModel getAnimatedModel2() {
+        return animatedModel2;
+    }
+
+
+    public PieChartModel getLivePieModel() {
+        int random1 = (int) (Math.random() * 1000);
+        int random2 = (int) (Math.random() * 1000);
+
+        livePieModel.getData().put("Candidate 1", random1);
+        livePieModel.getData().put("Candidate 2", random2);
+
+        livePieModel.setTitle("Votes");
+        livePieModel.setLegendPosition("ne");
+
+        return livePieModel;
+    }
+
+
+
+    private void createAreaModel() {
+        areaModel = new LineChartModel();
+
+        LineChartSeries boys = new LineChartSeries();
+        boys.setFill(true);
+        boys.setLabel("Boys");
+        boys.set("2004", 120);
+        boys.set("2005", 100);
+        boys.set("2006", 44);
+        boys.set("2007", 150);
+        boys.set("2008", 25);
+
+        LineChartSeries girls = new LineChartSeries();
+        girls.setFill(true);
+        girls.setLabel("Girls");
+        girls.set("2004", 52);
+        girls.set("2005", 60);
+        girls.set("2006", 110);
+        girls.set("2007", 90);
+        girls.set("2008", 120);
+
+        areaModel.addSeries(boys);
+        areaModel.addSeries(girls);
+
+        areaModel.setTitle("Area Chart");
+        areaModel.setLegendPosition("ne");
+        areaModel.setStacked(true);
+        areaModel.setShowPointLabels(true);
+
+        Axis xAxis = new CategoryAxis("Years");
+        areaModel.getAxes().put(AxisType.X, xAxis);
+        Axis yAxis = areaModel.getAxis(AxisType.Y);
+        yAxis.setLabel("Births");
+        yAxis.setMin(0);
+        yAxis.setMax(300);
+    }
+
+    private BarChartModel initBarModel() {
+        BarChartModel model = new BarChartModel();
+
+        ChartSeries boys = new ChartSeries();
+        boys.setLabel("Boys");
+        boys.set("2004", 120);
+        boys.set("2005", 100);
+        boys.set("2006", 44);
+        boys.set("2007", 150);
+        boys.set("2008", 25);
+
+        ChartSeries girls = new ChartSeries();
+        girls.setLabel("Girls");
+        girls.set("2004", 52);
+        girls.set("2005", 60);
+        girls.set("2006", 110);
+        girls.set("2007", 135);
+        girls.set("2008", 120);
+
+        model.addSeries(boys);
+        model.addSeries(girls);
+
+        return model;
+    }
+
+    private void createBarModels() {
+        createBarModel();
+        createHorizontalBarModel();
+    }
+
+    private void createBarModel() {
+        barModel = initBarModel();
+
+        barModel.setTitle("Bar Chart");
+        barModel.setLegendPosition("ne");
+
+        Axis xAxis = barModel.getAxis(AxisType.X);
+        xAxis.setLabel("Gender");
+
+        Axis yAxis = barModel.getAxis(AxisType.Y);
+        yAxis.setLabel("Births");
+        yAxis.setMin(0);
+        yAxis.setMax(200);
+    }
+
+    private void createHorizontalBarModel() {
+        horizontalBarModel = new HorizontalBarChartModel();
+
+        ChartSeries boys = new ChartSeries();
+        boys.setLabel("Boys");
+        boys.set("2004", 50);
+        boys.set("2005", 96);
+        boys.set("2006", 44);
+        boys.set("2007", 55);
+        boys.set("2008", 25);
+
+        ChartSeries girls = new ChartSeries();
+        girls.setLabel("Girls");
+        girls.set("2004", 52);
+        girls.set("2005", 60);
+        girls.set("2006", 82);
+        girls.set("2007", 35);
+        girls.set("2008", 120);
+
+        horizontalBarModel.addSeries(boys);
+        horizontalBarModel.addSeries(girls);
+
+        horizontalBarModel.setTitle("Horizontal and Stacked");
+        horizontalBarModel.setLegendPosition("e");
+        horizontalBarModel.setStacked(true);
+
+        Axis xAxis = horizontalBarModel.getAxis(AxisType.X);
+        xAxis.setLabel("Births");
+        xAxis.setMin(0);
+        xAxis.setMax(200);
+
+        Axis yAxis = horizontalBarModel.getAxis(AxisType.Y);
+        yAxis.setLabel("Gender");
+    }
+
+    private void createCombinedModel() {
+        combinedModel = new BarChartModel();
+
+        BarChartSeries boys = new BarChartSeries();
+        boys.setLabel("Boys");
+
+        boys.set("2004", 120);
+        boys.set("2005", 100);
+        boys.set("2006", 44);
+        boys.set("2007", 150);
+        boys.set("2008", 25);
+
+        LineChartSeries girls = new LineChartSeries();
+        girls.setLabel("Girls");
+
+        girls.set("2004", 52);
+        girls.set("2005", 60);
+        girls.set("2006", 110);
+        girls.set("2007", 135);
+        girls.set("2008", 120);
+
+        combinedModel.addSeries(boys);
+        combinedModel.addSeries(girls);
+
+        combinedModel.setTitle("Bar and Line");
+        combinedModel.setLegendPosition("ne");
+        combinedModel.setMouseoverHighlight(false);
+        combinedModel.setShowDatatip(false);
+        combinedModel.setShowPointLabels(true);
+        Axis yAxis = combinedModel.getAxis(AxisType.Y);
+        yAxis.setMin(0);
+        yAxis.setMax(200);
+    }
+
+    private void createMultiAxisModel() {
+        multiAxisModel = new LineChartModel();
+
+        BarChartSeries boys = new BarChartSeries();
+        boys.setLabel("Boys");
+
+        boys.set("2004", 120);
+        boys.set("2005", 100);
+        boys.set("2006", 44);
+        boys.set("2007", 150);
+        boys.set("2008", 25);
+
+        LineChartSeries girls = new LineChartSeries();
+        girls.setLabel("Girls");
+        girls.setXaxis(AxisType.X2);
+        girls.setYaxis(AxisType.Y2);
+
+        girls.set("A", 52);
+        girls.set("B", 60);
+        girls.set("C", 110);
+        girls.set("D", 135);
+        girls.set("E", 120);
+
+        multiAxisModel.addSeries(boys);
+        multiAxisModel.addSeries(girls);
+
+        multiAxisModel.setTitle("Multi Axis Chart");
+        multiAxisModel.setMouseoverHighlight(false);
+
+        multiAxisModel.getAxes().put(AxisType.X, new CategoryAxis("Years"));
+        multiAxisModel.getAxes().put(AxisType.X2, new CategoryAxis("Period"));
+
+        Axis yAxis = multiAxisModel.getAxis(AxisType.Y);
+        yAxis.setLabel("Birth");
+        yAxis.setMin(0);
+        yAxis.setMax(200);
+
+        Axis y2Axis = new LinearAxis("Number");
+        y2Axis.setMin(0);
+        y2Axis.setMax(200);
+
+        multiAxisModel.getAxes().put(AxisType.Y2, y2Axis);
+    }
+
+    private void createOhlcModels() {
+        createOhlcModel1();
+        createOhlcModel2();
+    }
+
+    private void createOhlcModel1() {
+        ohlcModel = new OhlcChartModel();
+
+        ohlcModel.add(new OhlcChartSeries(2007, 143.82, 144.56, 136.04, 136.97));
+        ohlcModel.add(new OhlcChartSeries(2008, 138.7, 139.68, 135.18, 135.4));
+        ohlcModel.add(new OhlcChartSeries(2009, 143.46, 144.66, 139.79, 140.02));
+        ohlcModel.add(new OhlcChartSeries(2010, 140.67, 143.56, 132.88, 142.44));
+        ohlcModel.add(new OhlcChartSeries(2011, 136.01, 139.5, 134.53, 139.48));
+        ohlcModel.add(new OhlcChartSeries(2012, 124.76, 135.9, 124.55, 135.81));
+        ohlcModel.add(new OhlcChartSeries(2013, 123.73, 129.31, 121.57, 122.5));
+
+        ohlcModel.setTitle("OHLC Chart");
+        ohlcModel.getAxis(AxisType.X).setLabel("Year");
+        ohlcModel.getAxis(AxisType.Y).setLabel("Price Change $K/Unit");
+    }
+
+    private void createOhlcModel2() {
+        ohlcModel2 = new OhlcChartModel();
+
+        for (int i = 1; i < 41; i++) {
+            ohlcModel2.add(new OhlcChartSeries(i, Math.random() * 80 + 80, Math.random() * 50 + 110, Math.random() * 20 + 80, Math.random() * 80 + 80));
+        }
+
+        ohlcModel2.setTitle("Candlestick");
+        ohlcModel2.setCandleStick(true);
+        ohlcModel2.getAxis(AxisType.X).setLabel("Sector");
+        ohlcModel2.getAxis(AxisType.Y).setLabel("Index Value");
+    }
+
+
+
+
+}
